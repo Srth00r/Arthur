e5b8874988def76d691a38f82e298477b2c338b0
1 <1@303-3>
2020-02-21 17:38:56 +0500

    Корректировки

--- WindowsFormsApp1/WindowsFormsApp1/Form2.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form2.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
